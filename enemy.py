eeea770c5355d7fa9007c1375f387f8646ffa462
Mark S. Brown <sandcalc@gmail.com>
2020-04-23 09:40:50 +0100

    Added adjacent priority for Enemy

--- enemy.py
+++ enemy.py
@@ -1,6 +1,6 @@
 import pyxel
 from random import shuffle
-from config import ENEMY_COLOUR, SHOT_COLOUR
+from config import ENEMY_COLOUR, SHOT_COLOUR, VERBOSE
 
 class Enemy:
     """
@@ -19,12 +19,9 @@ class Enemy:
         self.shots_fired = kwargs.get('shots_fired', [])
         self.size = kwargs.get('size', 10)
 
-    def next_move(self):
-        """
-        returns next move after all locations created and then shuffled
-        priority tiles (ie next to a win) are put to the front of the queue!
-        """
+        self._generate_all_moves()
 
+    def _generate_all_moves(self):
         self.potential_moves = []
         for i in range(self.board.N):
             for j in range(self.board.N):
@@ -32,6 +29,11 @@ class Enemy:
         
         shuffle(self.potential_moves)
 
+    def next_move(self):
+        """
+        returns next move after all locations created and then shuffled
+        priority tiles (ie next to a win) are put to the front of the queue!
+        """
         return self.potential_moves.pop(0)
 
     def draw(self):
@@ -57,10 +59,27 @@ class Enemy:
         
         self.i, self.j = shot_fired
 
-        if shot_fired in self.board.ships:
+        if shot_fired in self.board.ships:  # succcess!
             self.board.ships.remove(shot_fired)
             self.board.dead_ships.append(shot_fired)
+            self.prioritise_adjacent(*shot_fired)
             
+    def prioritise_adjacent(self, x, y):
+        """
+        Move shots adjacent to successful shot to front of queue
+        """
+        to_shift = []
+        L = 1
+        for index, (new_x, new_y) in enumerate(self.potential_moves):
+            if new_x in range(x-L, x+2*L, L) and new_y in range(y-L, y+2*L, L):
+                to_shift.append(index)
+
+        # Places all adjacent tiles at the front of the queue
+        for index in to_shift:
+            self.potential_moves.insert(0, self.potential_moves.pop(index))
+
+            if VERBOSE:
+                print("To the front of the queue :", self.potential_moves[0])
 
     def update(self):
         if not self.countdown:
